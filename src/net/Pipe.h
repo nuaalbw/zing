ca4dbef39272b40408fc111681ccea9cd728b6a1
nuaalbw <296273803@qq.com>
2021-11-22 22:29:55 +0800

    feat: add Pipe

--- src/net/Pipe.h
+++ src/net/Pipe.h
@@ -4,3 +4,31 @@
 	> Mail: nuaalbw@163.com 
 	> Created Time: Sun 21 Nov 2021 08:05:23 PM CST
  ************************************************************************/
+
+#ifndef ZING_NET_PIPE_H
+#define ZING_NET_PIPE_H
+
+namespace zing
+{
+namespace net
+{
+
+class Pipe
+{
+public:
+	bool create();
+	int write(void* buf, int len);
+	int read(void* buf, int len);
+	void close();
+
+	int readfd() const;
+	int writefd() const;
+
+private:
+	int pipefd_[2];
+};
+	
+} // namespace net
+} // namespace zing
+
+#endif
