39a2f6fd5dee4201dbd06ecb2844e2ea82fca178
nuaalbw <296273803@qq.com>
2022-02-02 21:44:44 +0800

    fix bug

--- zing/net/Channel.cpp
+++ zing/net/Channel.cpp
@@ -20,9 +20,9 @@ const int Channel::kNoneEvent = 0;
 const int Channel::kReadEvent = POLLIN | POLLPRI;
 const int Channel::kWriteEvent = POLLOUT;
 
-Channel::Channel(EventLoop* loop, int fd)
+Channel::Channel(EventLoop* loop, int fd__)
 	: loop_(loop), 
-		fd_(fd), 
+		fd_(fd__), 
 		events_(0), 
 		revents_(0), 
 		index_(-1), 
